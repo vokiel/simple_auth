07aa9c574f6dc37d41e6b54d209cb12d859bd103
Vokiel <vokiel@gmail.com>
2013-06-26 22:39:11 +0200

    Hanariu migration

---
+++ classes/Model/Auth/User/Tokens.php
@@ -0,0 +1,129 @@
+<?php namespace Model\Auth\User;
+
+/**
+ * User Token Model
+ *
+ * @package    SimpleAuth
+ * @author      thejw23
+ * @copyright    (c) 2010 thejw23
+ * @license    http://www.opensource.org/licenses/isc-license.txt
+ * @version    2.0
+ * @last change
+ *
+ * based on KohanaPHP Auth and Simple_Modeler
+ */
+
+class Tokens extends \Authmodeler\Core
+{
+
+  protected $table_name = 'auth_user_tokens';
+
+  protected $auto_trim = TRUE;
+
+  protected $data = array('id' => '',
+    'user_id' => '',
+    'expires' => '',
+    'time_stamp_created' => '',
+    'user_agent' => '',
+    'time_stamp' => '',
+    'token' => '');
+
+  protected $now;
+
+  /**
+   * Constructor
+   *
+   * @param integer $id unique token to be loaded
+   * @return void
+   */
+  public function __construct($id = FALSE)
+  {
+    parent::__construct();
+
+    $this->now = date('Y-m-d H:i:s');
+
+    if ($id != NULL AND is_string($id)) {
+      $this->load($id, 'token');
+
+      if ($this->loaded()) {
+        if ($this->data['expires'] < $this->now) {
+          $this->delete_expired();
+          $this->clear_data();
+          \Hanariu\Cookie::delete(\Hanariu::$config->load('simpleauth')->cookie_key);
+        }
+      } else {
+        \Hanariu\Cookie::delete(\Hanariu::$config->load('simpleauth')->cookie_key);
+      }
+    }
+  }
+
+  /**
+   * Overload saving to set the created time and to create a new token
+   * when the object is saved.
+   *
+   * @return void
+   */
+  public function save()
+  {
+    if ($this->data[$this->primary_key] == 0) {
+      //$this->data['created'] = $this->now;
+      $this->data['user_agent'] = sha1(\Hanariu\Request::user_agent('browser'));
+    }
+
+    $this->data['token'] = $this->create_token();
+
+    return parent::save();
+  }
+
+  /**
+   * Deletes all expired tokens.
+   *
+   * @return integer
+   */
+  public function delete_expired()
+  {
+    return \Hanariu\DB::delete($this->table_name)->where('expires', '<=', $this->now)->execute();
+  }
+
+
+  /**
+   * Deletes all expired tokens and the user old tokens for current user_agent.
+   *
+   * @param integer $id unique user id to delete tokens
+   * @return mixed
+   */
+  public function delete_user_tokens($id = 0, $all = FALSE)
+  {
+    $this->delete_expired();
+
+    if (intval($id) === 0)
+      return FALSE;
+
+    if ($all)
+      return \Hanariu\DB::delete($this->table_name)->where('user_id', '=', $id)->execute();
+    else
+      return \Hanariu\DB::delete($this->table_name)->where('user_id', '=', $id)->where('user_agent', '=', sha1(\Hanariu\Request::user_agent('browser')))->execute();
+  }
+
+  /**
+   * Finds a new unique token, using a loop to make sure that the token does
+   * not already exist in the database. This could potentially become an
+   * infinite loop, but the chances of that happening are very unlikely.
+   *
+   * @return  string
+   */
+  protected function create_token()
+  {
+    while (TRUE) {
+      // Create a random token
+      $token = \Hanariu\Text::random('alnum', 32);
+
+      // Make sure the token does not already exist
+      if (count(\Hanariu\DB::select('id')->from($this->table_name)->where('token', '=', $token)->execute()) === 0) {
+        // A unique token has been found
+        return $token;
+      }
+    }
+  }
+
+} // End User Tokens Model
---
+++ classes/Model/Auth/Users.php
@@ -0,0 +1,96 @@
+<?php namespace Model\Auth;
+
+/**
+ * User Model
+ *
+ * @package    SimpleAuth
+ * @author      thejw23
+ * @copyright    (c) 2010 thejw23
+ * @license    http://www.opensource.org/licenses/isc-license.txt
+ * @version    2.0
+ * @last change
+ *
+ * based on KohanaPHP Auth and Simple_Modeler
+ */
+
+class Users extends \Authmodeler\Core
+{
+
+  protected $table_name = 'auth_users';
+
+  protected $unique_field = '';
+  protected $second_field = '';
+  protected $password_field = '';
+
+  protected $data = array('id' => '',
+    'username' => '',
+    'password' => '',
+    'email' => '',
+    'logins' => '',
+    'admin' => '',
+    'moderator' => '',
+    'active' => '',
+    'active_to' => '',
+    'ip_address' => '',
+    'last_ip_address' => '',
+    'time_stamp' => '',
+    'last_time_stamp' => '',
+    'time_stamp_created' => '');
+
+  public $timestamp = array();
+
+  public function __construct($id = NULL)
+  {
+    parent::__construct($id);
+
+    $auth_config = \Hanariu::$config->load('simpleauth');
+
+    $this->unique_field = $auth_config['unique'];
+    $this->second_field = $auth_config['unique_second'];
+    $this->password_field = $auth_config['password'];
+  }
+
+  public function get_user($unique, $pass)
+  {
+    $data = \Hanariu\DB::select('*')->from($this->table_name)->where($this->unique_field, '=', $unique)->and_where($this->password_field, '=', $pass)->execute();
+
+    if (count($data) === 1 AND $data = $data->current()) {
+      $this->data_original = (array)$data;
+      $this->data = $this->data_original;
+    }
+  }
+
+  /**
+   * Check if username exists in database.
+   *
+   * @param string $name username to check
+   * @param string $second second username to check
+   * @return boolean
+   */
+  public function user_exists($name = NULL, $second = NULL)
+  {
+    if (!empty($second)) {
+      return count(\Hanariu\DB::select('id')->from($this->table_name)->where($this->unique_field, '=', $name)->or_where($this->second_field, '=', $second)->execute());
+    } else {
+      return count(\Hanariu\DB::select('id')->from($this->table_name)->where($this->unique_field, '=', $name)->execute());
+    }
+  }
+
+  public function save()
+  {
+    if ($this->is_sha1($this->password_field)) {
+      $password_field = $this->password_field;
+      $this->$password_field = \Simpleauth\Core::instance()->hash($this->$password_field);
+    }
+    if (!$this->is_sha1($this->{$this->password_field})) {
+      $this->{$this->password_field} = \Simpleauth\Core::instance()->hash($this->{$this->password_field});
+    }
+    return parent::save();
+  }
+
+  public static function is_sha1($str = '')
+  {
+    return (bool)preg_match('/^[0-9a-f]{40}$/i', $str);
+  }
+
+} // End Auth Users Model
